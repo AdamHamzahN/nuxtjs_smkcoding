b73cc42f608f8370a8d6949ab7f47bf90bc00e40
AdamHamzahN <adamhamzahofficial13@gmail.com>
2023-11-04 18:17:41 +0700

    perubahan 11/4/2023

--- components/dropdown.vue
+++ components/dropdown.vue
@@ -1,26 +1,32 @@
 <script lang="ts" setup>
-import { products } from '~/composables/constants/products';
-const selectedCategory = ref("");
-const allProducts = computed(() =>{
-    if (selectedCategory.value) {
-        return products.filter((item) => item.category === selectedCategory.value);
-    }
-    return products;
-});
+import { category } from "~/composables/constants/category";
+const isShow = ref(false);
+defineEmits(["selectedCategory"]);
 </script>
+
 <template>
-    <section>
-        <div class="container">
-            <div class="py-10">
-                <div class="mb-6 flex justify-end gap-6">
-                    <Dropdown @selected-category="selectedCategory = $event" />
-                </div>
-                <div class="flex gap-6 flex-wrap mx-auto">
-                    <template v-for="(item, index) in allProducts" :key="index">
-                        <CardsCardProduct :product="item" class="w-[calc(100%/4-18px)]" />
-                    </template>
-                </div>
-            </div>
+    <div class="relative select-none">
+        <div class="border border-primary flex items-center justify-center px-3 py-2
+rounded-lg gap-4 cursor-pointer w-full" @click="isShow = !isShow">
+            <span class="text-primary font-medium">Category</span>
+            <i :class="`ri-arrow-down-s-line text-primary text-xl text-bold transition
+duration-300 ${isShow ? 'rotate-180' : 'rotate-0'}`"></i>
         </div>
-    </section>
+        <!-- Dropdown menu -->
+        <div v-show="isShow" class="z-10 bg-white divide-y divide-gray-100 rounded-lg
+shadow-xl w-full absolute top-14">
+            <ul class="py-2 text-sm text-gray-700">
+                <li>
+                    <span class="block px-4 py-2 hover:bg-primary hover:text-white
+transition duration-200 cursor-pointer" @click="$emit('selectedCategory',
+    '')">All</span>
+                </li>
+                <li v-for="(item, index) in category" :key="index">
+                    <span class="block px-4 py-2 hover:bg-primary hover:text-white
+transition duration-200 cursor-pointer" @click="$emit('selectedCategory',
+    item.name)">{{ item?.name }}</span>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
